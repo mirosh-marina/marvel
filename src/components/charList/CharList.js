620484b67f99ac8ccca82631856a717ae01a29d2
Orion <serg.batumi2022@gmail.com>
2024-02-20 02:04:06 -0800

    fix: change charlist on my version

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -5,96 +5,89 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 import Spinner from "../spinner/Spinner";
 
 class CharList extends Component {
-  
-    
-    state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       charList: [],
       loading: true,
       error: false,
-      newItemLoading: false,
-      offset: 210,
-      charEnded: false
-  
+      nineChar: 210
+    };
   }
 
   marvelService = new MarvelService();
 
-  componentDidMount() {
-    this.onRequest();
+  async componentDidMount() {    
+    this.marvelService.getAllCharacters(this.state.nineChar)
+        .then(this.onCharListLoaded)
+        .catch(this.onError)
   }
 
-  onRequest = (offset) => {
-    this.onCharListLoading();
-    this.marvelService
-      .getAllCharacters(offset)
-      .then(this.onCharListLoaded)
-      .catch(this.onError);
-  };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.nineChar !== prevState.nineChar) {
+      this.setState({loading: true});
+      this.marvelService.getAllCharacters(this.state.nineChar)
+        .then(this.onCharListLoaded)
+        .catch(this.onError)
+    }
+  }
 
-  onCharListLoading = () => {
-    this.setState({
-      newItemLoading: true,
-    });
-  };
-
-  onCharListLoaded = (newCharList) => {
-    this.setState(({offset, charList}) => ({
-      charList: [...charList, ...newCharList],
-      loading: false,
-      newItemLoading: false,
-      // offset: offset+9
-    }));
-  };
+  onCharListLoaded = (charList) => {
+    this.setState({charList, loading: false})
+  }
 
   onError = () => {
-    this.setState({ error: true, loading: false });
-  };
+    this.setState({error: true, loading: false})
+  }
+
+  onNextNine = () => {
+    this.setState({nineChar: this.state.nineChar + 9})
+  }
 
   
 
-  allChar(arr) {    
+  allChar (arr) {
+    
+
     const elements = arr.map((item) => {
       let noImg = {};
-      if (item.noThumbnailOne) {
-        noImg = { objectFit: "contain", width: "235px" };
+      if(item.noThumbnailOne) {
+      noImg = {objectFit: 'contain', width: '235px'}
       } else if (item.noThumbnailTwo) {
-        noImg = { objectFit: "fill" };
+        noImg = {objectFit: 'fill'}
       } else {
         noImg = null;
-      }
+      }      
       return (
-        <li
-          key={item.id}
-          className="char__item"
-          onClick={() => this.props.onCharSelected(item.id)}
-        >
+        <li key={item.id} className="char__item" onClick={() => this.props.onCharSelected(item.id)}>
           <img style={noImg} src={item.thumbnail} alt={item.name} />
           <div className="char__name">{item.name}</div>
         </li>
       );
     });
-    return <ul className="char__grid">{elements}</ul>;
-  }
+    return (
+      <ul className="char__grid">{elements}</ul>
+    )
+  };
+
+
 
   render() {
-    const { charList, loading, error, offset, newItemLoading } = this.state;
+
+    const {charList, loading, error} = this.state;
 
     const items = this.allChar(charList);
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? items : null;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? items : null; 
 
     return (
       <div className="char__list">
         {errorMessage}
         {spinner}
         {content}
-        <button
-          disabled={newItemLoading}
-          onClick={() => this.onRequest(offset)}
-          className="button button__main button__long"
-        >
+        <button onClick={() => this.onNextNine()} className="button button__main button__long">
           <div className="inner">load more</div>
         </button>
       </div>
